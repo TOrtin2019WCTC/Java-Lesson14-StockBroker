0c85baed56888dadba7960c4f2392b01d050c8f8
tortin2019wctc <tortin@my.wctc.edu>
2019-05-15 13:23:32 -0500

    Completed deliverable

---
+++ src/command/Main.java
@@ -0,0 +1,19 @@
+package command;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stock abcStock = new Stock();
+
+        Order buyStockOrder = new BuyStock(abcStock);
+        Order sellStockOrder = new SellStock(abcStock);
+
+        Broker broker = new Broker();
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+     
+        broker.placeOrders();
+        broker.undoOrder(buyStockOrder);
+    }
+
+}
